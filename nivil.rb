803b5222e412655e5c3d40ad3288b0bf260e5f9c
Zate <zate75@gmail.com>
2011-01-17 17:14:21 -0800

    Removed all IVIL Parsing

--- nivil.rb
+++ nivil.rb
@@ -5,8 +5,6 @@ require 'optparse'
 require 'uri'
 require 'net/https'
 require 'rexml/document'
-require 'rexml/formatters/default'
-
 
 include REXML
 
@@ -15,8 +13,6 @@ options = {}
 
 optparse = OptionParser.new do |opts|
     opts.banner = "Nessus Wrapper for Seccubus v2\nUsage: ./nivil.rb [options] "
-    
-    
     opts.on('-u', '--user USER', 'Username to login to Nessus') do |username|
       options[:username] = username
     end
@@ -42,24 +38,15 @@ optparse = OptionParser.new do |opts|
     opts.on('-f', '--file INFILE', 'File of hosts to scan') do |file|
         options[:file] = file
     end
-    opts.on('-o', '--output OUTFILE', 'Name of IVIL output file') do |out|
-        options[:out] = out
-    end
     opts.on('--show-policies', 'Shows Server Policies') do
         options[:showpol] = true
     end
     opts.on('--show-reports', 'Shows Server Reports') do
         options[:showrpt] = true
     end
-    opts.on('-g', '--get-report RPTID', 'Download Report and Export to IVIL/Nessus V2.  If no output is given (-o) then it will just return status of that report.') do |rpt|
+    opts.on('-g', '--get-report RPTID', 'Download Report in Nessus V2 format.') do |rpt|
         options[:rptid] = rpt
     end
-    opts.on('-c', '--convert FILE', 'Local Nessus V2 file to convert') do |file|
-        options[:cfile] = file
-    end
-    #opts.on('-r', '--report RPTID', 'Report ID to get status of.') do |rptid|
-    #    options[:cfile] = file
-    #end
     case ARGV.length
     when 0
       puts opts
@@ -107,285 +94,6 @@ class NessusConnection
         end
         return res.body
     end
-end
-
-class NessusXMLStreamParser
-    
-    attr_accessor :on_found_host
-
-    def initialize(&block)
-        reset_state
-        on_found_host = block if block
-    end
-
-    def reset_state
-        @host = {'hname' => nil, 'addr' => nil, 'mac' => nil, 'os' => nil, 'ports' => [
-            'port' => {'port' => nil, 'svc_name'  => nil, 'proto' => nil, 'severity' => nil,
-            'nasl' => nil, 'description' => nil, 'cve' => [], 'bid' => [], 'xref' => [], 'msf' => nil } ] }
-        @state = :generic_state
-    end
-
-    def tag_start(name, attributes)
-        case name
-        when "tag"
-            if attributes['name'] == "mac-address"
-                @state = :is_mac
-            end
-            if attributes['name'] == "host-fqdn"
-                @state = :is_fqdn
-            end
-            if attributes['name'] == "ip-addr"
-                @state = :is_ip
-            end
-            if attributes['name'] == "host-ip"
-                @state = :is_ip
-            end
-            if attributes['name'] == "operating-system"
-                @state = :is_os
-            end
-        when "ReportHost"
-            @host['hname'] = attributes['name']
-        when "ReportItem"
-            @cve = Array.new
-            @bid = Array.new
-            @xref = Array.new
-            @see_also = Array.new
-            @x = Hash.new
-            @x['nasl'] = attributes['pluginID']
-            @x['port'] = attributes['port']
-            @x['proto'] = attributes['protocol']
-            @x['svc_name'] = attributes['svc_name']
-            @x['severity'] = attributes['severity']
-        when "description"
-            @state = :is_desc
-        when "synopsis"
-            @state = :is_synop
-        when "plugin_output"
-            @state = :is_plugin_ouput
-        when "see_also"
-            @state = :see_also
-        when "cve"
-            @state = :is_cve
-        when "bid"
-            @state = :is_bid
-        when "xref"
-            @state = :is_xref
-        when "solution"
-            @state = :is_solution
-        when "metasploit_name"
-            @state = :msf
-        end
-    end
-    
-    def text(str)
-        case @state
-        when :is_fqdn
-            @host['hname'] = str
-        when :is_ip
-            @host['addr'] = str
-        when :is_os
-            @host['os'] = str
-        when :is_mac
-            @host['mac'] = str
-        when :is_desc
-            @x['description'] = str
-        when :is_synop
-            @x['synopsis'] = str
-        when :is_plugin_output
-            @x['plugin_ouput'] = str
-        when :see_also
-            @see_also.push str
-        when :is_cve
-            @cve.push str
-        when :is_bid
-            @bid.push str
-        when :is_xref
-            @xref.push str
-        when :msf
-            @x['msf'] = str
-        end
-    end
-
-    def tag_end(name)
-        case name
-        when "ReportHost"
-            on_found_host.call(@host) if on_found_host
-            reset_state
-        when "ReportItem"
-            @x['cve'] = @cve
-            @x['bid'] = @bid
-            @x['xref'] = @xref
-            @host['ports'].push @x
-        end
-        @state = :generic_state
-    end
-
-    # We don't need these methods, but they're necessary to keep REXML happy
-    #
-    def xmldecl(version, encoding, standalone); end
-    def cdata; end
-    def comment(str); end
-    def instruction(name, instruction); end
-    def attlist; end
-end # end of parser class
-
-#<IVIL version=0.2>
-#    <addressee>
-#        <program>Seccubus|…
-#        <programSpecificData>
-#            <ScanID>
-#            <ScanID>
-#        </programSpecificData>
-#    </addressee>
-#    <sender>
-#        <scanner_type>Nessus|Nikto|MSF|OpenVAS|Qualis|...
-#        <version>
-#        <timestamp>YYYYMMDDHHMMSS</
-#    <sender/>
-#    <hosts>
-#        <host>
-#            <ip>
-#	    <findings>
-#	        <finding>
-#                    <port>
-#                    <id>
-#                    <severity>
-#                    <finding_txt>
-#                    <references>
-#                        <cve>
-#                        <bid>
-#                        <osvdb>
-#                        <url>
-#                        <msf>
-#                    </references>
-#                </finding>
-#            </findings>
-#        </host>
-#    </hosts>
-#</ivil>
-
-def parse_ivil(content)
-    parser = NessusXMLStreamParser.new
-    ivil = Document.new
-    pretty = Formatters::Default.new
-    ivil << XMLDecl.new
-    ivil.add_element( "ivil", {"version" => "0.2"})
-    #Could add function to set this.
-    #addressee = ivil.root.add_element("addressee")
-    #program = addressee.add_element("program")
-    #program.text = "Seccubus"
-    #programspecific = addressee.add_element("programSpecificData")
-    #scanid = programspecific.add_element("ScanID")
-    #scanid.text = "1"
-    sender = ivil.root.add_element("sender")
-    scannertype = sender.add_element("scanner_type")
-    scannertype.text = "Nessus"
-    scanner_version = sender.add_element("version")
-    scanner_version.text = "4.2"
-    timestamp = sender.add_element("timestamp")
-    time = Time.new
-    timestamp.text = time.strftime("%Y%m%d%H%M%S")
-    hosts = ivil.root.add_element("hosts")
-    
-    parser.on_found_host = Proc.new { |host|
-        hostx = hosts.add_element("host")
-        addr = host['addr'] || host['hname']
-        addr.gsub!(/[\n\r]/," or ") if addr
-        ipx = hostx.add_element("ip")
-        ipx.text = addr
-        os = host['os']
-        os.gsub!(/[\n\r]/," or ") if os
-        
-        hname = host['hname']
-        hname.gsub!(/[\n\r]/," or ") if hname
-        
-        mac = host['mac']
-        mac.gsub!(/[\n\r]/," or ") if mac
-        findings = hostx.add_element("findings")
-        host['ports'].each do |item|
-            finding = findings.add_element("finding")
-            
-            exp = []
-            msf = nil
-            nasl = item['nasl']
-            #port = number/protocol
-            #if pluginid = 0 then make it a portscanner
-            port = item['port'].to_s
-            proto = item['proto'] || "tcp"
-            portx = finding.add_element("port")
-            portx.text = "#{port}/#{proto}"
-            
-            
-            name = item['svc_name']
-            #id will be plugin ID
-            id = finding.add_element("id")
-            id.text = nasl
-            
-            severity = item['severity']
-            sevx = finding.add_element("severity")
-            sevx.text = severity
-            
-            description = item['description']
-            synopsis = item['synopsis']
-            plugin_ouput = item['plugin_output']
-            finding_txt = finding.add_element("finding_txt")
-            if nasl.to_i == 0
-                if port.to_i == 0
-                    finding_txt.text = "Host Is Alive"
-                else
-                    finding_txt.text = "Open Port Found"
-                end
-            else
-                desc = "Description:
-#{description if description}
-"
-                syn = "Synopsis:
-#{synopsis if synopsis}
-"
-                plugout = "Plugin Ouput:
-#{plugin_output if plugin_ouput}"
-                finding_txt.text = "#{desc if description}#{syn if synopsis}#{plugout if plugin_ouput}"
-            end
-            
-            refs = finding.add_element("references")
-            
-            cve = item['cve']
-            
-            if cve
-                cve.each do |stuff|
-                    cvex = refs.add_element("cve")
-                    cvex.text = stuff
-                end
-            end
-            
-            bid = item['bid']
-            if bid
-                bid.each do |stuff|
-                    bidx = refs.add_element("bid")
-                    bidx.text = stuff
-                end
-            end
-            
-            xref = item['xref']
-            if xref
-                xref.each do |stuff|
-                    xrefx = refs.add_element("xref")
-                    xrefx.text = stuff
-                end
-            end
-            
-            msf = item['msf']
-            if msf
-                msfx = refs.add_element("msf")
-                msfx.text = msf
-            end 
-        end
-    }
-    REXML::Document.parse_stream(content, parser)
-    out = String.new
-    pretty.write(ivil, out)
-    return out
-    
 end
 
 def show_policy(options)
@@ -450,14 +158,11 @@ def get_report(options)
     post_data = { "token" => @token, "report" => options[:rptid]  }
     stuff = @n.connect(uri, post_data)
     
-    if options[:out]
-        File.open("#{options[:out]}.nessus", 'w') {|f| f.write(stuff) }
-        puts("#{options[:out]}.nessus written.")
-        ivil = parse_ivil(stuff)
-        File.open("#{options[:out]}.ivil", 'w') {|f| f.write(ivil) }
-        puts("#{options[:out]}.ivil written.")
+    if options[:rptid]
+        File.open("#{options[:rptid]}.nessus", 'w') {|f| f.write(stuff) }
         exit
-        
+    else
+        puts("Error: No Report Specified")
     end
 end
 
@@ -473,34 +178,12 @@ def rpt_status(options)
             if scan.elements['status'].text == "running"
                 now = scan.elements['completion_current'].text
                 total = scan.elements['completion_total'].text
-                #percent = (now.to_f / total.to_f) * 100
                 puts("#{scan.elements['status'].text}|#{now}|#{total}")
             end
         end
     }
 end
-
-if options[:cfile]
-    if !options[:out]
-        puts("Missing output filename (-o)")
-        exit
-    end
-    stuff = ''
-    puts("importing #{options[:cfile]}")
-    fc = File.open("#{options[:cfile]}", 'r')
-    fc.each_line do |line|
-        stuff += line
-    end
-    ivil = parse_ivil(stuff)
-    File.open("#{options[:out]}.ivil", 'w') {|f| f.write(ivil) }
-    puts("#{options[:out]}.ivil written.")
-    exit
-end
-
 @n = NessusConnection.new(options[:username], options[:passwd], options[:server])
-#ok lets check we have everything.
-
-
 
 if options[:showpol]
     login(options)
@@ -516,15 +199,10 @@ end
 
 if options[:rptid]
     login(options)
-    if !options[:out]
-        rpt_status(options)
-        exit
-    end
     get_report(options)
     exit
 end
 
-#login
 login(options)
 
 ##verify policy
